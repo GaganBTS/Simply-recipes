8a62494cb707a1f9f8832e1778d48efb9a0439e7
Gagandeep Singh <69230886+GaganBTS@users.noreply.github.com>
2022-10-17 17:41:36 +0530

    Add files via upload

---
+++ SimplyRecipes/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+
+from .views import *
+urlpatterns = [
+    path('',homepage,name='home'),
+    path('recipes',all_recipes,name='all_recipes'),
+    path('about',about,name='about'),
+    path('contact',contact,name='contact'),
+    path('privacy-policy',privacy,name='privacy'),
+    path('terms-and-conditions',terms,name='terms'),
+    path('recipes/<slug:slug>',single_recipe,name='single_recipe')
+]
