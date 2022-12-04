08ce532217ea2c8b66feec03333788e1a61c7513
Gagandeep Singh <69230886+GaganBTS@users.noreply.github.com>
2022-12-04 07:54:15 +0530

    Update settings.py

--- recipes_project/settings.py
+++ recipes_project/settings.py
@@ -79,7 +79,10 @@ WSGI_APPLICATION = 'recipes_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
